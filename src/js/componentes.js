07f33ed5f4d22e0b4378af96c8a8c49ceb8fe357
Lucas <lucas_repetto@hotmail.com>
2021-04-28 12:54:58 -0400

    css configuration

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -6,7 +6,7 @@ export const saludar = ( nombre ) => {
     console.log('Creando etiqueta h1, en el HTML');
 
     const h1 = document.createElement('h1');
-    h1.innerText = `Hola, ${ nombre }, como estas?`;
+    h1.innerText = `Hola ${ nombre }, como estas?`;
 
     document.body.append( h1 );
 }
